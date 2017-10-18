4267b3f8af8c304b9a51de407a00510f80e1386b
Joshua Hansen <hansenlj@us.ibm.com>
2017-10-18 07:03:29 -0600

    :crystal_ball: Undoing `dist` push

--- dist/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.webhookKey = 'f3HqyRey51BeJWWjrq4I-AP9hy_IosZZ3IZfDR5lGya';
-exports.coords = {
-    lat: 40.038152,
-    lng: -105.0382077,
-};
