2d280c44ac1aff8194c7a4e7f0e7e96244367326
Joshua Hansen <hansenlj@us.ibm.com>
2017-11-22 15:43:17 -0700

    :art: Fixed non-camelcase typo

--- src/conditions/pins.ts
+++ src/conditions/pins.ts
@@ -1,5 +1,6 @@
-import Socketcondition from '../condition-socket';
+import SocketCondition from '../condition-socket';
 import io from '../io';
 
-export const desk_lamp = new Socketcondition('desk_lamp', io);
-export const salt_lamp = new Socketcondition('salt_lamp', io);
+// Lamps
+export const desk_lamp = new SocketCondition('desk_lamp', io);
+export const salt_lamp = new SocketCondition('salt_lamp', io);
